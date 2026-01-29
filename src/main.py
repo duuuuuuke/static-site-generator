30eec1d482286880f98b8ebafa50215dfad88b44
duke <dukebai8383@gmail.com>
2026-01-29 14:44:36 +1000

    remove public

--- src/main.py
+++ src/main.py
@@ -1,11 +1,41 @@
+import os
+import shutil
 from textnode import TextNode, TextType
 
 
 def main():
-    text_node = TextNode(
-        "This is some anchor text", TextType.LINK, "https://www.boot.dev"
-    )
-    print(text_node)
+    copy_content()
+
+
+def clear_public():
+    for item in os.listdir("public"):
+        path = os.path.join("public", item)
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            shutil.rmtree(path)
+
+
+def copy_content():
+    shutil.rmtree("public", ignore_errors=True)
+    files = find_files("static")
+    for file in files:
+        path = file.split("/")
+        if len(path) > 2:
+            new_path = f'public/{"/".join(path[1:-1])}'
+            os.makedirs(new_path, exist_ok=True)
+        shutil.copy(file, f'public/{"/".join(path[1:])}')
+
+
+def find_files(folder):
+    res = []
+    for name in os.listdir(folder):
+        path = os.path.join(folder, name)
+        if os.path.isfile(path):
+            res.append(path)
+        else:
+            res.extend(find_files(path))
+    return res
 
 
 main()
