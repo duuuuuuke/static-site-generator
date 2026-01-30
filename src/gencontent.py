cdca31008e20132c8961a36971a7284705d1b4f2
duke <dukebai8383@gmail.com>
2026-01-30 11:36:16 +1000

    Fixed basepath

--- src/gencontent.py
+++ src/gencontent.py
@@ -11,7 +11,7 @@ def extract_title(markdown):
     raise ValueError("no title found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown = ""
     with open(from_path, "r") as f:
@@ -24,8 +24,8 @@ def generate_page(from_path, template_path, dest_path):
     html_content = node.to_html()
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html_content)
-    template = template.replace('href="/', 'href="{basepath}')
-    template = template.replace('src="/', 'src="{basepath}')
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
 
     dest_dir_path = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir_path):
@@ -34,12 +34,12 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
